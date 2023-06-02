6fedc4bbbdc653267ca746b4209a08631a70b0cf
jasonn361 <79726650+jasonn361@users.noreply.github.com>
2023-06-02 11:25:23 -0700

    Change ColorColumn highlight

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,4 +2,4 @@ require("huy.config.keymappings")
 require("huy.config.options")
 require("huy.config.lazy")
 
-vim.cmd([[highlight ColorColumn ctermbg=0 guibg=#b695ff]])
+vim.cmd([[highlight ColorColumn ctermbg=0 guibg=#57437a]])
